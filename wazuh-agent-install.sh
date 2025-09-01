1268e7c977de40b7153dedffa81c92cf762ccdf6
Sayandip Chatterjee <31971141+sayandip-chatterjee@users.noreply.github.com>
2025-09-02 00:35:15 +0530

    Update wazuh-agent-install.sh

--- wazuh-agent-install.sh
+++ wazuh-agent-install.sh
@@ -5,7 +5,7 @@
 # Run this script as root or with sudo
 
 curl -s https://packages.wazuh.com/key/GPG-KEY-WAZUH | gpg --no-default-keyring --keyring gnupg-ring:/usr/share/keyrings/wazuh.gpg --import && chmod 644 /usr/share/keyrings/wazuh.gpg
-echo "deb [signed-by=/usr/share/keyrings/wazuh.gpg] https://packages.wazuh.com/4.12/apt/ stable main" | tee -a /etc/apt/sources.list.d/wazuh.list
+echo "deb [signed-by=/usr/share/keyrings/wazuh.gpg] https://packages.wazuh.com/4.x/apt/ stable main" | tee -a /etc/apt/sources.list.d/wazuh.list
 apt-get update
 WAZUH_MANAGER="WS_IP" apt-get install wazuh-agent
 systemctl daemon-reload
